4b4ea49c5a6ec1654b39cb121fe9b6f66627d90f
Wang Chunfei <1041875526@qq.com>
2018-06-29 23:09:57 +0800

    fix path

--- judger/judger_test.go
+++ judger/judger_test.go
@@ -10,7 +10,7 @@ import (
 //	args, env []string
 func TestJudger(t *testing.T) {
 	result := Judger(1000, 2000, 128*1024*1024, 200, 10000, 32*1024*1024, 0, 0, 0,
-		"../main", "in.txt", "out.txt", "echo.out", "judger.log", "c_cpp",
+		"../main", "/in.txt", "/root/out.txt", "/root/echo.out", "/root/judger.log", "c_cpp",
 		[]string{""}, []string{"foo=bar"})
 	fmt.Println(result)
 	t.Log(result)
